90ed8df9c360176762f5cb14b12e3f9c8326d404
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-06-26 12:07:28 -0400

    finished everything

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int x = 20;
+        double y = 40.5;
+        long p = 30;
+        float q = 10.60f;
+        // int z = x + y; (1) // Error; cannot convert from double to int.
+        double z = x + y;
+        System.out.println("Sum of two numbers: " + z);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // long r = p - q; // (2) // Error; cannot convert from float to long.
+        float r = p - q;
+        System.out.println("Subtraction of two numbers: " + r);
+
+
     }
 }
